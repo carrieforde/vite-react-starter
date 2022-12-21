2d0f70c5f3c232632660212da11b0ebdc7858782
Carrie Forde <carrie@carrieforde.com>
2022-12-20 21:28:30 -0800

    add baseweb

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
+import { BaseProvider, LightTheme } from "baseui";
+import { App } from "components";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "components";
-import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import "./styles.css";
+
+const engine = new Styletron();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>
 );
